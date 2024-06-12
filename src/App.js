f3f6b4ca3b806b45a2ac2b051d623ecb8d0850e2
Animesh <animeshjare26@gmail.com>
2024-06-12 20:48:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./routes/Home";
 import Faq from "./routes/Faq";
 import PastSeminar from "./routes/PastSeminar";
